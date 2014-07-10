9769f02e229d3733edd72fd899954efee1e25ee6
Mitchell Malinin <mitchellmalinin@Mitchells-MacBook-Pro.local>
2014-07-10 11:54:29 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/Character.m
+++ FlappyBird.spritebuilder/Source/Character.m
@@ -18,9 +18,9 @@
     self.physicsBody.collisionType = @"character";
 }
 
-- (void)flap
-{
-    [self.physicsBody applyImpulse:ccp(0, 400.f)];
-}
+//- (void)flap
+//{
+//    [self.physicsBody applyImpulse:ccp(0, 400.f)];
+//}
 
 @end
